232610167f3f2f16d2c4654777605e0890b5e823
Obloqulov Shoyim <92427513+ShoyimObloqulov@users.noreply.github.com>
2023-10-18 17:00:54 +0500

    data

---
+++ app/Livewire/IndexBase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class IndexBase extends Component
+{
+    public $base;
+    public $search;
+    public function render()
+    {
+        $this->base = \App\Models\Base::where('name','LIKE','%'.$this->search.'%')
+            ->orWhere('desc','LIKE','%'.$this->search.'%')
+            ->get();
+        return view('livewire.index-base');
+    }
+}
---
+++ storage/framework/views/35f608f034cb639a2d79011ff2a08f61.php
@@ -0,0 +1,42 @@
+<div>
+    
+    <div class="object-center m-4">
+        <form class="flex items-center">
+            <label for="simple-search" class="sr-only">Izlash</label>
+            <div class="relative w-full">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"/>
+                    </svg>
+                </div>
+                <input wire:model.live="search" type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search ..." required>
+            </div>
+        </form>
+    </div>
+    <div class="m-4">
+        <div class="row flex flex-wrap">
+            <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $base; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $b): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-2">
+                    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <img class="" src="https://img.freepik.com/free-vector/stack-colorful-books_74855-314.jpg?size=626&ext=jpg&ga=GA1.1.1142390609.1697629977&semt=ais" alt="" />
+                        </a>
+                        <div class="p-5">
+                            <a href="#">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo e($b->name); ?></h5>
+                            </a>
+                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400"><?php echo e($b->desc); ?></p>
+                            <a href="<?php echo e(route('base.show',$b->id)); ?>" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Read more
+                                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?> <!--[if ENDBLOCK]><![endif]-->
+        </div>
+    </div>
+</div>
+<?php /**PATH D:\OSPanel\domains\mindshelf\resources\views/livewire/index-base.blade.php ENDPATH**/ ?>
